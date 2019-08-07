248e5b98b84ae764a4aeeff9e7f6d1d347bd9b34
Jason Ruan <jasonruan@pursuit.org>
2019-08-07 12:13:58 -0400

    Initial commit

---
+++ Auto Layout Lab/Auto Layout LabTests/Auto_Layout_LabTests.swift
@@ -0,0 +1,34 @@
+//
+//  Auto_Layout_LabTests.swift
+//  Auto Layout LabTests
+//
+//  Created by Jason Ruan on 8/7/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import XCTest
+@testable import Auto_Layout_Lab
+
+class Auto_Layout_LabTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
