b89c6f801c21aabf3306c27f368ebc55e0de45b4
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-08-07 22:22:49 -0400

    adds solution to question 1, 2, 3 and 4 for iphone devices portrait mode only

---
+++ autoLayoutLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoLayoutLab
+//
+//  Created by Mariel Hoepelman on 8/7/19.
+//  Copyright © 2019 Mariel Hoepelman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
