16867a0e285ff26ef9efbe793c04b59feb702bc5
Kevin Natera <mycityneedsme@Kevins-MacBook-Pro.local>
2019-08-10 18:24:05 -0400

    .

---
+++ Auto-Layout-Lab/Auto-Layout-Lab/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Auto-Layout-Lab
+//
+//  Created by Kevin Natera on 8/7/19.
+//  Copyright © 2019 Kevin Natera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
