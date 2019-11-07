bcc0101da06cbb3282840b4c6a4acb6d2910a511
CameronRivera <cameronrivera@pursuit.org>
2019-11-07 18:15:42 -0500

    Completed Questions 1 through 5

---
+++ AutoLayoutLab/AutoLayoutLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutLab
+//
+//  Created by Cameron Rivera on 11/7/19.
+//  Copyright © 2019 Cameron Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
