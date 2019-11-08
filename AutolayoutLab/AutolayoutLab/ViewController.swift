25be5dcbedf5f4cd5677470e21649fb82fc8000c
LubaKaper <liubovkaper@pursuit.com>
2019-11-08 10:11:38 -0500

    1 querstion

---
+++ AutolayoutLab/AutolayoutLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutolayoutLab
+//
+//  Created by Liubov Kaper  on 11/7/19.
+//  Copyright © 2019 Luba Kaper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
